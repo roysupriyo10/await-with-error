62cfea1fe74cea7bd6cf003f677dc45ddc5a239e
Supriyo Roy <roysupriyo10@gmail.com>
2024-03-30 09:22:42 +0530

    lib for getting errors as values

---
+++ index.ts
@@ -0,0 +1,15 @@
+export function withError<T, U = Error>(
+  promise: Promise<T>,
+  errorExt?: object,
+): Promise<[U, undefined] | [null, T]> {
+  return promise
+    .then<[null, T]>((data: T) => [null, data])
+    .catch((err: U) => {
+      if (errorExt) {
+        const parsedError = Object.assign({}, err, errorExt);
+        return [parsedError, undefined];
+      }
+
+      return [err, undefined];
+    });
+}
